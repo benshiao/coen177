f5f0bd3fe08618c177cb4787243d453c2620c4d2
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-03-31 18:53:51 -0700

    Create calcCircle.sh

---
+++ calcCircle.sh
@@ -0,0 +1,34 @@
+# Name: Benjamin Shiao
+# Date: 3/31/20
+# Title: Lab1 – Unix/Linux Commands and Basic Shell Programming
+# Description: This program computes a simple sh file that calculates the area of a circle given a radius and repeats on a given Yes/No. Works only for user=bshiao
+#Sample shell programs for Lab assignment
+#!/bin/sh
+echo Executing $0
+echo $(/bin/ls | wc -l) files
+wc -l $(/bin/ls)
+echo "HOME="$HOME
+echo "USER="$USER
+echo "PATH="$PATH
+echo "PWD="$PWD
+echo "\$\$"=$$
+user=`whoami`
+numusers=`who | wc -l`
+echo "Hi $user! There are $numusers users logged on."
+if [ $user = "bshiao" ]
+then
+       echo "Now you can proceed!"
+else
+       echo "Check who logged in!"
+       exit 1
+fi
+response="Yes"
+while [ $response != "No" ]
+do
+       echo "Enter radius of circle: "
+       read radius
+       area=`expr $radius \* $radius \* $PI`
+       echo "The area of the circle is $area"
+       echo "Would you like to repeat for another circle [Yes/No]?"
+       read response
+done
