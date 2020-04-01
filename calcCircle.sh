b3762fa3b31b099af11c2f137828d9e22d5c21a0
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-03-31 19:02:35 -0700

    Update calcCircle.sh

--- calcCircle.sh
+++ calcCircle.sh
@@ -27,7 +27,8 @@ while [ $response != "No" ]
 do
        echo "Enter radius of circle: "
        read radius
-       area='expr $radius \* $radius'
+       PI=3.14159
+       area=`expr $PI \* $radius \* $radius`
        echo "The area of the circle is $area"
        echo "Would you like to repeat for another circle [Yes/No]?"
        read response
