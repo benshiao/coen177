c02b3e390041745f560e4c26af88c73c0ccccf4b
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-07 20:29:20 -0700

    Update lab2step5.c

--- lab2/lab2step5.c
+++ lab2/lab2step5.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
    printf("\n Before forking.\n");
    pid1 = fork();
    pid2 = fork();
-   pid3 = fork();
+   if(pid1){ fork(); }
   printf("\t \t \t new process---%d -- %d \n", getpid(), getppid());
    wait(NULL);
    return 0;
