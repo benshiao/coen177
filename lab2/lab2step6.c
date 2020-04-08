9d7bbd05c13ad796df812e9e9e5d453d81e17050
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-07 19:28:50 -0700

    Update lab2step6.c

--- lab2/lab2step6.c
+++ lab2/lab2step6.c
@@ -1,7 +1,7 @@
 /* Name: Benjamin Shiao
 # Date: 4/7/20
 # Title: Lab2 – Programming in C and use of Systems Calls
-# Description: Original unedited file given in step 1.
+# Description: Step 6, edited file given in step 1 to run with threads instead of processes
 */
 
 /*Sample C program for Lab assignment 1*/
@@ -11,6 +11,11 @@
 #include <stdlib.h>     /* atoi */
 #include <errno.h>      /* errno */
 /* main function with command-line arguments to pass */
+
+void threadFunc(){
+   printf("i am a thread");
+}
+
 int main(int argc, char *argv[]) {
    pid_t  pid;
    int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
