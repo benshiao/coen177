e26eb9e67e4898a55ea33b81871bc53d3ecf9c5a
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-07 18:50:52 -0700

    Update lab2step5.c

--- lab2/lab2step5.c
+++ lab2/lab2step5.c
@@ -15,23 +15,14 @@ int main(int argc, char *argv[]) {
    pid_t  pid;
    int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
    printf("\n Before forking.\n");
-   pid = fork();
+   pid1 = fork();
+   pid2 = fork();
+   pid3 = fork();
    if (pid == -1) {
        fprintf(stderr, "can't fork, error %d\n", errno);
    }
-   if (pid){
-       // Parent process
-       for (i=0;i<2;i++) {
-           printf("\t \t \t Parent Process %d \n",i);
-           usleep(n);
-       }
-   }
-   else{
-       // Child process
-       for (i=0;i<2;i++) {
-           printf("Child process %d\n",i);
-           usleep(n);
-       }
-   }
+   i++;
+  printf("\t \t \t PID number %d \n", i);
+  usleep(n);
    return 0;
 }
