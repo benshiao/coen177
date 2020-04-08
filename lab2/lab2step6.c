287de55e02bed20e3cf2a02d8a1ac8b1727d01b2
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-07 21:16:02 -0700

    Update lab2step6.c

--- lab2/lab2step6.c
+++ lab2/lab2step6.c
@@ -10,33 +10,31 @@
 #include <unistd.h>     /* fork */
 #include <stdlib.h>     /* atoi */
 #include <errno.h>      /* errno */
+#include <pthread.h>
 /* main function with command-line arguments to pass */
 
-void threadFunc(){
-   printf("i am a thread");
+void *threadFuncParent(int n){
+   for (int i=0;i<100;i++) {
+     printf("\t \t \t Parent Process %d \n",i);
+     usleep(n);
+   }  
+   return;
+}
+void *threadFuncChild(int n){
+   for (int i=0;i<100;i++) {
+     printf("\t \t \t Child Process %d \n",i);
+     usleep(n);
+   }  
+   return;
 }
 
 int main(int argc, char *argv[]) {
-   pid_t  pid;
-   int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
-   printf("\n Before forking.\n");
-   pid = fork();
-   if (pid == -1) {
-       fprintf(stderr, "can't fork, error %d\n", errno);
-   }
-   if (pid){
-       // Parent process
-       for (i=0;i<100;i++) {
-           printf("\t \t \t Parent Process %d \n",i);
-           usleep(n);
-       }
-   }
-   else{
-       // Child process
-       for (i=0;i<100;i++) {
-           printf("Child process %d\n",i);
-           usleep(n);
-       }
-   }
+   int n = atoi(argv[1]); // n microseconds to input from keyboard for delay
+   printf("\n Before Thread.\n");
+   pthread_t thread_id;
+   pthread_create(&thread_id, NULL, threadFuncParent, &n);
+   pthread_join(thread_id, NULL);
+   printf("\n After Thread.\n");
+   
    return 0;
 }
