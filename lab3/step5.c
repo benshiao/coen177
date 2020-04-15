f7c0c8804b852290c14e4309be3d41ce6e654e75
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-14 20:33:28 -0700

    Update step5.c

--- lab3/step5.c
+++ lab3/step5.c
@@ -4,36 +4,45 @@
 # Description: Step five, a C program to implement the producer-consumer message communication using pipes. 
 */
 
-/*Sample C program for Lab assignment 3*/
+/**************************
+*   Lab3 - pipe()
+**************************/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
-//main
-int main() {
-   int fds[2];
-   pipe(fds);
-   /*child 1 duplicates downstream into stdin */
-   if (fork() == 0) {
-       dup2(fds[0], 0);
-       close(fds[1]);
-     //cant put in 2 arguements like below, 1st program (grep), 2nd (grep), 3rd is pointer to list of strings( where root is)
-
-
-       execlp("grep", "grep", "root", NULL);//grep searches for smth with root in it
-   }
-   /*child 2 duplicates upstream into stdout */
-   else if (fork() == 0) {
-       dup2(fds[1], 1);
+// main
+int main(int argc,char *argv[]){
+  int  fds[2];
+  char buff[60];
+  int count;
+  int i;
+  pipe(fds);
+  if (fork()==0){
+      printf("\nWriter on the upstream end of the pipe -> %d arguments \n",argc);
+      
+      dup2(fds[1], 1);
+      close(fds[0]);        
+      execlp("grep", "grep", "root", NULL);
+      
+      exit(0);
+  }
+  else if(fork()==0){
+      printf("\nReader on the downstream end of the pipe \n");
+     
+      dup2(fds[1], 1);
        close(fds[0]);
       //takes input that is "/etc/passwd" and pass into below
-       execlp("cat", "cat", "/etc/passwd", 0);//cat prints out file contents
+       execlp("cat", "cat", buff, 0);//cat prints out file contents
+     
+      exit(0);
    }
-   else {  /*parent closes both ends and wait for children*/
-       close(fds[0]);
-       close(fds[1]);
-       wait(0);
-       wait(0);
-   }
+  else{    
+     close(fds[0]);
+     close(fds[1]);
+     wait(0);
+     wait(0);
+  }
 return 0;
 }
