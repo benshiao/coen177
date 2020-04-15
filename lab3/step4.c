7ed1be0a7d3dc95da13dea725d7bd2c9a9d79930
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-14 19:05:01 -0700

    Update step4.c

--- lab3/step4.c
+++ lab3/step4.c
@@ -4,52 +4,33 @@
 # Description: Step four, a C program meant to implement the shell command:  cat /etc/passwd | grep root
 */
 
-/**************************
-*   Lab3 - pipe()
-**************************/
+/*Sample C program for Lab assignment 3*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/wait.h>
-// main
-int main(int argc,char *argv[]){
-  int  fds[2];
-  char buff[60];
-  int count;
-  int i;
-  pipe(fds);
-  if (fork()==0){
-      printf("\nWriter on the upstream end of the pipe -> %d arguments \n",argc);
-      //close(fds[0]);
-      //for(i=0;i<argc;i++){
-      //    write(fds[1],argv[i],strlen(argv[i]));
-      //}
-      //new code:
-      
-      dup2(fds[1], 1);
-      close(fds[0]);        
-      execlp("cat /etc/passwd | grep root", "cat /etc/passwd | grep root", 0); 
-      
-      exit(0);
-  }
-  else if(fork()==0){
-      printf("\nReader on the downstream end of the pipe \n");
-      close(fds[1]);
-      while((count=read(fds[0],buff,60))>0){
-          for(i=0;i<count;i++){
-              write(1,buff+i,1);
-              write(1," ",1);
-          }
-          printf("\n");
-      }
-      exit(0);
+//main
+int main() {
+   int fds[2];
+   pipe(fds);
+   /*child 1 duplicates downstream into stdin */
+   if (fork() == 0) {
+       dup2(fds[0], 0);
+       close(fds[1]);
+     //cant put in 2 arguements like below, 1st program (grep), 2nd (grep), 3rd is pointer to list of strings( where root is)
+       execlp("grep root", "grep root", 0);//grep searches for smth with root in it
    }
-  else{    
-     close(fds[0]);
-     close(fds[1]);
-     wait(0);
-     wait(0);
-  }
+   /*child 2 duplicates upstream into stdout */
+   else if (fork() == 0) {
+       dup2(fds[1], 1);
+       close(fds[0]);
+       execlp("cat /etc/passwd", "cat /etc/passwd", 0);//cat prints out file contents
+   }
+   else {  /*parent closes both ends and wait for children*/
+       close(fds[0]);
+       close(fds[1]);
+       wait(0);
+       wait(0);
+   }
 return 0;
 }
