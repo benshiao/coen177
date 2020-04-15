4e1bd3505ede3ea18ea2ff21ba7f11902bb211a2
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-14 18:11:37 -0700

    Create step4.c

---
+++ lab3/step4.c
@@ -0,0 +1,55 @@
+/* Name: Benjamin Shiao
+# Date: 4/14/20
+# Title: Lab3 – Pthreads and inter-process Communication – Pipes
+# Description: Step four, a C program meant to implement the shell command:  cat /etc/passwd | grep root
+*/
+
+/**************************
+*   Lab3 - pipe()
+**************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+// main
+int main(int argc,char *argv[]){
+  int  fds[2];
+  char buff[60];
+  int count;
+  int i;
+  pipe(fds);
+  if (fork()==0){
+      printf("\nWriter on the upstream end of the pipe -> %d arguments \n",argc);
+      //close(fds[0]);
+      //for(i=0;i<argc;i++){
+      //    write(fds[1],argv[i],strlen(argv[i]));
+      //}
+      //new code:
+      
+      dup2(fds[1], 1);
+      close(fds[0]);        
+      execlp("cat /etc/passwd | grep root", "cat /etc/passwd | grep root", 0); 
+      
+      exit(0);
+  }
+  else if(fork()==0){
+      printf("\nReader on the downstream end of the pipe \n");
+      close(fds[1]);
+      while((count=read(fds[0],buff,60))>0){
+          for(i=0;i<count;i++){
+              write(1,buff+i,1);
+              write(1," ",1);
+          }
+          printf("\n");
+      }
+      exit(0);
+   }
+  else{    
+     close(fds[0]);
+     close(fds[1]);
+     wait(0);
+     wait(0);
+  }
+return 0;
+}
