b12031fdaabf2480579562bee3b443894dd86a9c
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-16 21:15:02 -0700

    Update step5.c

--- lab3/step5.c
+++ lab3/step5.c
@@ -38,15 +38,12 @@ int main(int argc,char *argv[]){
       printf("\nReader on the downstream end of the pipe \n");
       //close(fds[1]);
     
-//       while((count=read(fds[0],buff,60))>0){
-//           for(i=0;i<count;i++){
-//               write(1,buff+i,1);
-//           }
-//           printf("\n");
-//       }  
-    for(i=1;i<argc;i++){
-         write(fds[1],argv[i],strlen(argv[i]));
-      }
+      while((count=read(fds[0],buff,60))>0){
+          for(i=0;i<count;i++){
+              write(1,buff+i,1);
+          }
+          printf("\n");
+      }  
     
     dup2(fds[1], 1);
       close(fds[0]);
