c8a48fda8bc4746b59642295ae32037dd860b2f9
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-16 21:17:24 -0700

    Update step5.c

--- lab3/step5.c
+++ lab3/step5.c
@@ -39,9 +39,9 @@ int main(int argc,char *argv[]){
       //close(fds[1]);
     
       while((count=read(fds[0],buff,60))>0){
-          for(i=0;i<count;i++){
-              write(1,buff+i,1);
-            printf("hi-");
+//           for(i=0;i<count;i++){
+//               write(1,buff+i,1);
+//             printf("hi-");
             printf(buff+i);
           }
           printf("\n");
