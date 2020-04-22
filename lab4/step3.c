a6ee63781cd46ed7345277079e77f7ae2ad0bd98
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-21 18:36:32 -0700

    Update step3.c

--- lab4/step3.c
+++ lab4/step3.c
@@ -14,12 +14,34 @@
 #define NTHREADS 20
 
 void *go(void *);
-pthread_t threads[NTHREADS];
+
+int M = 3;
+int N = 3;
+int L = 3;
+pthread_t threads[M];
+
+double matrixA[N][M];
+double matrixB[M][L];
 
 int main() {
-    static int i;
-    for (i = 0; i < NTHREADS; i++)  
-        pthread_create(&threads[i], NULL, go, &i);
+    static int i;	
+//filling in matrix A---------------------
+    srand(time(NULL));
+    for (int i = 0; i < N; i++)
+	for (int j = 0; j < M; j++)
+	    matrixA[i][j] = rand();
+	
+//filling in matrix B--------------------	
+    srand(time(NULL));
+    for (int i = 0; i < M; i++)
+	for (int j = 0; j < L; j++)
+	    matrixB[i][j] = rand();
+
+//-------------------------------	
+    for (i = 0; i < NTHREADS; i++){
+       int *i2 = malloc(sizeof(*i2));
+       *i2 = i;
+       pthread_create(&threads[i], NULL, go, i2);
     for (i = 0; i < NTHREADS; i++) {
         pthread_join(threads[i],NULL);
 	    printf("Thread %d returned \n", i);
