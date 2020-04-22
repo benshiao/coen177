07d9864f1e1d7f0242d28fe352c6441464a5a862
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-21 18:11:42 -0700

    Update and rename step3.c to step2.c

---
+++ lab4/step2.c
@@ -0,0 +1,28 @@
+/* Name: Benjamin Shiao
+# Date: 4/21/20
+# Title: Lab4 – Developing multi-threaded applications
+# Description: Step two threadhello code without thread join code loop.
+*/
+/**************************************************************
+*	threadHello.c - simple multi-threaded program.            *
+*   compile with: >gcc -lp threadHello.c -o threadHello       *                                              *
+**************************************************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NTHREADS 20
+
+void *go(void *);
+pthread_t threads[NTHREADS];
+
+int main() {
+    static int i;
+    for (i = 0; i < NTHREADS; i++)  
+        pthread_create(&threads[i], NULL, go, &i);
+    printf("Main thread done.\n");
+}
+void *go(void *arg) {
+    printf("Hello from thread %d with thread ID %d \n", *(int *)arg, (int)pthread_self());
+    return (NULL);
+}
