d0d01bf9b067fd048fe7d64f987c7240e1feefc5
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-28 18:16:25 -0700

    Create step3.c

---
+++ lab5/step3.c
@@ -0,0 +1,78 @@
+//Shared data: semaphore full, empty, mutex;
+//pool of n buffers, each can hold one item
+//mutex provides mutual exclusion to the buffer pool
+//empty and full count the number of empty and full buffers
+//Initially: full = 0, empty = n, mutex = 1
+/* Name: Benjamin Shiao
+# Date: 4/28/20
+# Title: Lab5 – Synchronization using semaphores, lock, and condition variables
+# Description: Modified version of file given for step1 to run using mutex locks instead of semaphores.
+*/
+// Thread Sychronization
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h> 
+#include <fcntl.h>
+#include <semaphore.h> 
+
+#define NTHREADS 10
+pthread_t threads[NTHREADS];
+sem_t *mutex; 
+ pthread_mutex_t lock;
+
+void* go(void* arg) { 
+  //sem_wait(mutex); //entry section
+  pthread_mutex_lock(&lock);
+  printf("Thread %d Entered Critical Section..\n", (int)arg); //critical section 
+   sleep(1); 
+  //sem_post(mutex); //exit section
+   pthread_mutex_unlock(&lock);
+  return (NULL);
+} 
+
+int main() { 
+//mutex = sem_open("mutex", O_CREAT, 0644, 1);
+  pthread_mutex_init(&lock, NULL);
+static int i;
+for (i = 0; i < NTHREADS; i++)  
+   pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
+for (i = 0; i < NTHREADS; i++) {
+   pthread_join(threads[i],NULL);
+   printf("\t\t\tThread %d returned \n", i);
+   }
+printf("Main thread done.\n");
+//sem_unlink("mutex"); 
+  pthread_mutex_destroy(&mutex);
+  return 0; 
+} 
+
+
+
+//Producer thread
+do {
+   ...
+   produce next item
+    ...
+   wait(empty);
+   wait(mutex);
+    ...
+   add the item to buffer
+    ...
+   signal(mutex);
+   signal(full);
+} while (1);
+
+//Consumer thread
+do {
+   wait(full)
+   wait(mutex);
+    ...
+   remove next item from buffer
+    ...
+   signal(mutex);
+   signal(empty);
+    ...
+   consume the item
+    ...
+} while (1);
