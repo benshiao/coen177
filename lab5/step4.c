43df9ca85d4bb68af8477d0e86c60239a1d38359
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-28 19:47:45 -0700

    Update step4.c

--- lab5/step4.c
+++ lab5/step4.c
@@ -2,7 +2,8 @@
 /* Name: Benjamin Shiao
 # Date: 4/28/20
 # Title: Lab5 – Synchronization using semaphores, lock, and condition variables
-# Description: Program to solve producer consumer problem using semaphores. Five producers and Five consumer threads are made.
+# Description: Program to solve producer consumer problem using semaphores. 
+#Five producers and Five consumer threads are made.
 */
 // Thread Sychronization
 
@@ -18,35 +19,58 @@ int buffer[10] = {11,22,33,44,55,66,77,88,99,100};
 sem_t *mutex; 
 sem_t *full; 
 sem_t *empty; 
+pthread_mutex_t lock;
+/*
+*lock = 
+  pthread_mutex_lock(&lock);
+  unlock
+pthread_mutex_unlock(&lock);
 
+*/
 void* consume(void* arg) { 
  do{
- sem_wait(full);
-  sem_wait(mutex); //entry section
-  printf("Thread %d Entered Critical Section.. Consuming %d\n", (int)arg, buffer[(int)arg]); //critical section 
+  pthread_mutex_lock(&lock); //entry section
+  while(/*buffer is full*/){
+    pthread_cond_wait(empty, mutex);
+  }
+  printf("Adding the %d item to buffer, %d\n", (int)arg, buffer[(int)arg]); //critical section 
    sleep(1); 
-  sem_post(mutex); //exit section
-  sem_post(empty); //exit section
+  
+  
+  pthread_cond_signal(full);
+  pthread_mutex_unlock(&lock);
   
  }while(1);
   return (NULL);
 } 
 void* produce(void* arg) { 
  do{
-  sem_wait(empty);
-  sem_wait(mutex); //entry section
-  printf("Thread %d Entered Critical Section.. producing item %d\n", (int)arg,buffer[(int)arg]); //critical section 
+  pthread_mutex_lock(&lock);
+  while(/*buffer is empty*/){
+    pthread_cond_wait(full, mutex);
+  }
+  printf("Removing %d item, %d\n", (int)arg,buffer[(int)arg]); //critical section 
    sleep(1); 
-  sem_post(mutex);
-  sem_post(full); //exit section
+  
+  
+  pthread_cond_signal(empty);
+  pthread_mutex_unlock(&lock);
+  
  }while(1);
   return (NULL);
 }
 
 int main() { 
-mutex = sem_open("mutex", O_CREAT, 0644, 1);
+ pthread_mutex_destroy(&mutex);
+  pthread_mutex_init(&lock, NULL);
+ sem_unlink("full"); 
+sem_unlink("empty"); 
+sem_unlink("mutex"); 
+full = sem_open("mutex", O_CREAT, 0644, 1);
 full = sem_open("full", O_CREAT, 0644, 0);
 empty = sem_open("empty", O_CREAT, 0644, 10);
+ 
+ 
 static int i;
 for (i = 0; i < NTHREADS; i++){ //can try seperate numbers, like more than 1 con and 1 prod
    if(i%2==0)
@@ -59,7 +83,6 @@ for (i = 0; i < NTHREADS; i++) {
    printf("\t\t\tThread %d returned \n", i);
    }
 printf("Main thread done.\n");
-sem_unlink("mutex"); 
   return 0; 
 } 
 
