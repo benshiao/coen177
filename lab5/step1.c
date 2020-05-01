986a8b749c9ece1e087351742fe4309591184a3b
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-30 17:37:36 -0700

    Update step1.c

--- lab5/step1.c
+++ lab5/step1.c
@@ -27,14 +27,14 @@ int main() {
   
 sem_unlink("mutex"); 
 mutex = sem_open("mutex", O_CREAT, 0644, 1);
-static int i;
-for (i = 0; i < NTHREADS; i++)  
-   pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
-for (i = 0; i < NTHREADS; i++) {
-   pthread_join(threads[i],NULL);
-   printf("\t\t\tThread %d returned \n", i);
-   }
-printf("Main thread done.\n");
-sem_unlink("mutex"); 
+// static int i;
+// for (i = 0; i < NTHREADS; i++)  
+//    pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
+// for (i = 0; i < NTHREADS; i++) {
+//    pthread_join(threads[i],NULL);
+//    printf("\t\t\tThread %d returned \n", i);
+//    }
+// printf("Main thread done.\n");
+// sem_unlink("mutex"); 
 return 0; 
 } 
