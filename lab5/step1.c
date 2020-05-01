37951a9f69867d5234df6cd6ec7f57eec821b4bc
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-04-30 17:38:55 -0700

    Update step1.c

--- lab5/step1.c
+++ lab5/step1.c
@@ -13,28 +13,28 @@
 
 #define NTHREADS 10
 pthread_t threads[NTHREADS];
-sem_t *mutex; 
+sem_t *mutex2; 
 
 void* go(void* arg) { 
-  sem_wait(mutex); //entry section
+  sem_wait(mutex2); //entry section
   printf("Thread %d Entered Critical Section..\n", (int)(size_t)arg); //critical section 
    sleep(1); 
-  sem_post(mutex); //exit section 
+  sem_post(mutex2); //exit section 
   return (NULL);
 } 
 
 int main() { 
   
-sem_unlink("mutex"); 
-mutex = sem_open("mutex", O_CREAT, 0644, 1);
-// static int i;
-// for (i = 0; i < NTHREADS; i++)  
-//    pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
-// for (i = 0; i < NTHREADS; i++) {
-//    pthread_join(threads[i],NULL);
-//    printf("\t\t\tThread %d returned \n", i);
-//    }
-// printf("Main thread done.\n");
-// sem_unlink("mutex"); 
+sem_unlink("mutex2"); 
+mutex2 = sem_open("mutex2", O_CREAT, 0644, 1);
+static int i;
+for (i = 0; i < NTHREADS; i++)  
+   pthread_create(&threads[i], NULL, go, (void *)(size_t)i);
+for (i = 0; i < NTHREADS; i++) {
+   pthread_join(threads[i],NULL);
+   printf("\t\t\tThread %d returned \n", i);
+   }
+printf("Main thread done.\n");
+sem_unlink("mutex2"); 
 return 0; 
 } 
