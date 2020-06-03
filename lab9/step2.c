10f2f6b51ec5fb3865a059315983eaa9af540e1d
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 20:19:18 -0700

    Update step2.c

--- lab9/step2.c
+++ lab9/step2.c
@@ -6,11 +6,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char buffer[10000];
-FILE *fp;
-fp = fopen(argv[1], "rb");
-while (fread(buffer, sizeof(buffer), 1, fp)){
+int main(int argc, char *argv[]) { 
+    char buffer[10000];
+    FILE *fp;
+    fp = fopen(argv[1], "rb");
+    while (fread(buffer, sizeof(buffer), 1, fp)){
+    }
+    fclose(fp);
+
 }
-fclose(fp);
-
-
