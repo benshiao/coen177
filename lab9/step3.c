cee431a0f51ca01b1498c1373f0305992599b672
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:55:53 -0700

    Update step3.c

--- lab9/step3.c
+++ lab9/step3.c
@@ -3,7 +3,7 @@
 # Title: Lab9 
 # Description: Step 3 code that takes in 2 args for buffer size and file name and then reads them.
 */
-char buffer[stoi(argv[2])];
+char buffer[atoi(argv[2])];
 FILE *fp;
 fp = fopen(argv[1], "rb");
 while (fread(buffer, sizeof(buffer), 1, fp)){
