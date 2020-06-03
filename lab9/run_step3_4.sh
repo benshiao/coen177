8d19f59af42c1f9d9aad1a300175ac9c89fbebfb
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:42:21 -0700

    Update run_step3_4.sh

--- lab9/run_step3_4.sh
+++ lab9/run_step3_4.sh
@@ -1,9 +1,9 @@
   
-/* Name: Benjamin Shiao
+#Name: Benjamin Shiao
 # Date: 6/2/20
 # Title: Lab9 
 # Description: Step 3 and step 4 sh code that runs all 4 txt files and reads them for the different buffer sizes
-*/
+
 
 
 for file in file1.txt file2.txt file3.txt file4.txt file5.txt
