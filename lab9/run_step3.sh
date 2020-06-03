23e4dca96e102c777ccf9e5ab08e5f96ca49cf4e
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:33:00 -0700

    Create run_step3.sh

---
+++ lab9/run_step3.sh
@@ -0,0 +1,17 @@
+  
+/* Name: Benjamin Shiao
+# Date: 6/2/20
+# Title: Lab9 
+# Description: Step 3 code that runs all 4 txt files and reads them for the different buffer sizes
+*/
+
+
+for file in file1.txt file2.txt file3.txt file4.txt file5.txt
+do
+            for buffer in 100 1000 10000 100000
+            do
+                   echo "Step3 $file $buffer"
+                   time ./Step3 $file $buffer
+                   echo " "
+            done
+done
