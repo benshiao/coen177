5805013024fb1f23c79eebcc518f656cb021199e
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:53:44 -0700

    Update step5.c

--- lab9/step5.c
+++ lab9/step5.c
@@ -22,17 +22,17 @@ fp = fopen(argv[1], "rb");
 
 wp = fopen( <idk> );
 /*
-static int i;
-for (i = 0; i < NTHREADS; i++){ //can try seperate numbers, like more than 1 con and 1 prod
-   if(i%2==0)
-    pthread_create(&threads[i], NULL, produce, (void *)(size_t)i);
-   else
-    pthread_create(&threads[i], NULL, consume, (void *)(size_t)i);
+
+int main() { 
+      static int i;
+      for (i = 0; i < argv[3]; i++){
+          pthread_create(&threads[i], NULL, write_func, (void *)(size_t)i);
+      }
+      for (i = 0; i < argv[3]; i++) {
+         pthread_join(threads[i],NULL);
+         printf("\t\t\tThread %d returned \n", i);
+      }
 }
-for (i = 0; i < NTHREADS; i++) {
-   pthread_join(threads[i],NULL);
-   printf("\t\t\tThread %d returned \n", i);
-   }
    */
 
 while (fread(buffer, sizeof(buffer), 1, fp)){
