4638b997bde4f9256c8f2c00d5f782dc4ec6fb40
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:56:21 -0700

    Update run_step3_4.sh

--- lab9/run_step3_4.sh
+++ lab9/run_step3_4.sh
@@ -6,7 +6,7 @@
 
 
 
-for file in file1.txt file2.txt file3.txt file4.txt file5.txt
+for file in file1.txt file2.txt file3.txt file4.txt 
 do
             for buffer in 100 1000 10000 100000
             do
