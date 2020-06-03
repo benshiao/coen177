65e9e53e7e54be8fc7d30c4fb4adfb2ca6ed2bb3
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:31:00 -0700

    Update step2.c

--- lab9/step2.c
+++ lab9/step2.c
@@ -1,4 +1,8 @@
-
+/* Name: Benjamin Shiao
+# Date: 6/2/20
+# Title: Lab9 
+# Description: Step 2 C code that reads a file with a buffer size 10,000
+*/
 char buffer[10000];
 FILE *fp;
 fp = fopen(argv[1], "rb");
