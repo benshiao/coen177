150d033e7c5a5438a8a485da77e2d2e80a399266
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:37:58 -0700

    Create step4.c

---
+++ lab9/step4.c
@@ -0,0 +1,19 @@
+/* Name: Benjamin Shiao
+# Date: 6/2/20
+# Title: Lab9 
+# Description: Step 4 C code that takes in 2 args for buffer size and file name and then reads and WRITEs using them.
+*/
+
+char buffer[stoi(argv[2])];
+FILE *fp;
+FILE *wp;
+fp = fopen(argv[1], "rb");
+
+
+wp = fopen(char *strcat(char *argv[1], const char *src);
+
+
+while (fread(buffer, sizeof(buffer), 1, fp)){
+    fwrite(buffer,sizeof(buffer),1, <MAKE FILE HERE?>);
+}
+fclose(fp);
