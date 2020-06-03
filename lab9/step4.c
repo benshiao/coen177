a42461bb8127b3d06c3467ea0c9ef9bce9430859
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 19:03:28 -0700

    Update step4.c

--- lab9/step4.c
+++ lab9/step4.c
@@ -4,13 +4,16 @@
 # Description: Step 4 C code that takes in 2 args for buffer size and file name and then reads and WRITEs using them.
 */
 
-char buffer[stoi(argv[2])];
+char buffer[atoi(argv[2])];
 FILE *fp;
 FILE *wp;
 fp = fopen(argv[1], "rb");
 
 
-wp = fopen( <idk> );
+char *str2 = malloc(strlen(argv[1]+5));
+strcpy(str2, argv[1]);
+
+wp = fopen(strcat(str2, ".out"), 1, "wb");
 
 
 while (fread(buffer, sizeof(buffer), 1, fp)){
