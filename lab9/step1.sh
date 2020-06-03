ff540940183d3bfbb6f4a81b6b07a6c58975f760
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:41:45 -0700

    Update step1.sh

--- lab9/step1.sh
+++ lab9/step1.sh
@@ -1,8 +1,8 @@
-/* Name: Benjamin Shiao
+# Name: Benjamin Shiao
 # Date: 6/2/20
 # Title: Lab9 
 # Description: Step 1 code that creates 4 txt files with random bytes for the sizes seen below
-*/
+#
 #!/bin/bash
 # cat /dev/random | head -c <bytecount>
 cat /dev/random | head -c 10000000 > file3.txt
