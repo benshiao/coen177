b17f45f6d665914deb6de280eb31ab0db24fe846
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-06-02 18:42:14 -0700

    Update run_step2.sh

--- lab9/run_step2.sh
+++ lab9/run_step2.sh
@@ -1,8 +1,8 @@
-/* Name: Benjamin Shiao
+# Name: Benjamin Shiao
 # Date: 6/2/20
 # Title: Lab9 
 # Description: Step 2 code that runs all 4 txt files and reads them with the standard buffer size
-*/
+
 for file in file1.txt file2.txt file3.txt file4.txt
 do
    echo "Step2 $file"
